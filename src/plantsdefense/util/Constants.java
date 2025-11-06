58c4e834b320b09e1e5500741c2a726e1bb33342
ThoaCSE <nguyenhuynhkthoa@gmail.com>
2025-11-06 14:33:34 +0700

    Add window, fps and grid settings

--- src/plantsdefense/util/Constants.java
+++ src/plantsdefense/util/Constants.java
@@ -1,4 +1,17 @@
 package plantsdefense.util;
 
 public class Constants {
+    private Constants() {};
+
+    //Window setting
+    public static final int window_width = 1200;
+    public static final int window_height = 700;
+
+    //FPS setting
+    public static final int FPS = 60;
+
+    //Grid setting
+    public static final int tile_size = 75;
+    public static final int cols = 16;
+    public static final int rows = 9;
 }
